f07efe3c6c4be6358a00c758888a879d5490835c
Olivia SERENELLI-PESIN <olivia.serenelli.pesin@gmail.com>
2019-04-16 15:18:22 +0200

    les boutons oui et non apparaissent, les boites sont normalement bien placees

--- troll.c
+++ troll.c
@@ -16,30 +16,57 @@ int main (int argc, char *argv[])
     //Declaration
     
     GtkWidget *window;
+    GtkWidget *box_principal;
     GtkWidget *box1;
     GtkWidget *box2;
+    GtkWidget *button_test;
     GtkWidget *button_o;    
     GtkWidget *button_n;
+   // GtkWidget *label;
 
     gtk_init (&argc, &argv);
-//*CRER*//
+
+
+    //creation de la fenetre 
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    //changement titre fenetre 
     gtk_window_set_title (GTK_WINDOW (window), "ATTENTION ALERTE CRITIQUE !");
 
+    //Affichage de la fenetre 
+    gtk_widget_show (window);
+
+
+    //Definition des boxs
+        //Box Principale qui contient tout
+    box_principal = gtk_vbox_new(FALSE, 0);
         //Box du Message 
     box1 = gtk_vbox_new(FALSE, 0);
         //Box boutons "Oui - Non "
     box2 = gtk_hbox_new(FALSE, 0);  
 
-    gtk_container_add (GTK_CONTAINER (window), box1);
+    //Definitions des boutons 
+    
+    button_o = gtk_button_new_with_label ("Oui");
+    button_n = gtk_button_new_with_label ("Non");
+
+    // Mise en place des box 1 et 2 dans la box principales
+    gtk_container_add (GTK_CONTAINER (window), box_principal);
+
+    gtk_box_pack_start(GTK_BOX(box_principal), box1, TRUE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box_principal), box2, TRUE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box2), button_o, TRUE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box2), button_n, TRUE, FALSE, 0);
 
 
-//*RELIER*//
 
-
-//*AFFICHER*//
-
-    gtk_widget_show (window);
+ //  gtk_widget_show (); 
+   gtk_widget_show (button_o); 
+   gtk_widget_show (button_n); 
+   gtk_widget_show (box_principal); 
+   gtk_widget_show (box1); 
+   gtk_widget_show (box2); 
+   gtk_widget_show (window); 
+    
     gtk_main ();
     return 0;
 }
