f21784a2aab82e43fe27c41204e2ff0c1cfb40cf
Olivia SERENELLI-PESIN <olivia.serenelli.pesin@gmail.com>
2019-04-19 10:13:00 +0200

    Boutons qui changent OK

--- troll.c
+++ troll.c
@@ -10,35 +10,33 @@ void destroy (GtkWidget *widget, gpointer data)
 
 void oui  (GtkWidget *widget, gpointer data)
 {
-   g_print ("oui\n");
-   gtk_button_set_label( (GtkButton*) widget, "Coucou" ) ;  
-   gtk_button_set_label( (GtkButton*) data, "Olivia" ) ;  
-    //GtkWidget *button_o;    
-    
-    //Definitions des boutons  
-   // button_o = gtk_button_new_with_label ("Non");
-  //  button_o = gtk_button_get_label ("Non");
-}
+   gchar* bouton;
+   gchar* bouton2;
+   bouton = gtk_button_get_label ((GtkButton*) widget);
+   bouton2 = gtk_button_get_label ((GtkButton*) data);
+   
 
+  // g_print ("%s\n",bouton2);
+  // g_print ("%s\n",bouton);
+
+/*PROBLEME :  Caractere non UTF8 qui s'incrementent au bout de deux changements. 
+  On pense que c'est du au fait qu'on change le label du bouton alors que c'est une 
+  variable constante a la base. 
+  A CHERCHER MAIS PAS TRES IMPORTANT ! */
+ // gtk_button_set_label( (GtkButton*) widget, bouton2 ) ;  
+ // gtk_button_set_label( (GtkButton*) data, bouton ) ;  
+
+    
+   //En attendant, on remplace par ca, qui fonctionne tout aussi bien : 
+
+     gtk_button_set_label( (GtkButton*) widget, "Oui" ) ; 
+     gtk_button_set_label( (GtkButton*) data, "Non" ) ;  
+
+}
 
 static gboolean mouse(GtkWidget *widget,gpointer user_data) {
-
-
-g_print("bouge\n") ;
-
+    g_print("bouge\n") ;
 }
-
-
-//void non  (GtkWidget *widget, gpointer data)
-//{
-    //g_print ("non\n");
-   
-  //  GtkWidget *button_n;    
-    
-    //Definitions des boutons  
-//    button_n = gtk_button_new_with_label ("oui");
-//}
-
 
 
 
@@ -53,11 +51,14 @@ int main (int argc, char *argv[])
     GtkWidget *box_principal;
     GtkWidget *box1;
     GtkWidget *box2;
+    GtkWidget *box_up;
     GtkWidget *button_o;    
     GtkWidget *button_n;
     GtkWidget *label;
     GtkWidget *image;
     GtkWidget *barre;
+    GtkWidget *button_quit;
+
 
     gtk_init (&argc, &argv);
 
@@ -66,7 +67,7 @@ int main (int argc, char *argv[])
     //creation de la fenetre 
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     //changement titre fenetre 
-    gtk_window_set_title (GTK_WINDOW (window), "ATTENTION ALERTE CRITIQUE !");
+    gtk_window_set_title (GTK_WINDOW (window), "BlobFish");
 
     //Affichage de la fenetre 
     gtk_widget_show (window);
@@ -79,11 +80,22 @@ int main (int argc, char *argv[])
     box1 = gtk_box_new( GTK_ORIENTATION_VERTICAL, 0);
         //Box boutons "Oui - Non "
     box2 = gtk_box_new( GTK_ORIENTATION_HORIZONTAL, 0);
+        //Box Up
+    box_up = gtk_box_new( GTK_ORIENTATION_HORIZONTAL, 0);
+ 
     
 
-    //Definitions des boutons  
-    button_o = gtk_button_new_with_label ("Oui");
-    button_n = gtk_button_new_with_label ("Non");
+    //Definitions des boutons 
+
+    button_o = gtk_button_new ();
+    gtk_button_set_label ((GtkButton*)button_o,"Oui");
+    button_n = gtk_button_new ();
+    gtk_button_set_label ((GtkButton*)button_n,"Non");
+
+  //  button_o = gtk_button_new_with_label ("Oui");
+  //  button_n = gtk_button_new_with_label ("Non");
+    button_quit = gtk_button_new ();
+    //gtk_button_set_image (button_quit, "/image/blob.png");
     label = gtk_label_new("FAIS LE BASIOU");
     
     //Image 
@@ -98,6 +110,7 @@ int main (int argc, char *argv[])
     gtk_container_add (GTK_CONTAINER (window), box_principal);
 
     // Mise en place des box 1 et 2 dans la box principales
+    gtk_box_pack_start(GTK_BOX(box_principal), box_up, TRUE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(box_principal), box1, TRUE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(box_principal), box2, TRUE, FALSE, 0);
     // Mise en place du texte dans la box 1 
@@ -111,8 +124,9 @@ int main (int argc, char *argv[])
    
 //FONCTIONS DES ELEMENTS 
     // Appelle d'une fonction si on clique dessus
-     g_signal_connect (button_o, "clicked", G_CALLBACK(oui), button_n);
-     g_signal_connect (button_n, "enter",  G_CALLBACK(mouse), NULL);
+     g_signal_connect (button_o, "enter", G_CALLBACK(oui), button_n);
+     g_signal_connect (button_n, "enter", G_CALLBACK(oui), button_o);
+//     g_signal_connect (button_n, "enter",  G_CALLBACK(mouse), NULL);
 //     g_signal_connect (GTK_OBJECT (button_n), "enter", GTK_SIGNAL_FUNC(mouse), NULL);
 
 
@@ -126,6 +140,8 @@ int main (int argc, char *argv[])
    gtk_widget_show (box2); 
    gtk_widget_show (window); 
    gtk_widget_show (barre);
+   gtk_widget_show (box_up);
+
     
     gtk_main ();
     return 0;
